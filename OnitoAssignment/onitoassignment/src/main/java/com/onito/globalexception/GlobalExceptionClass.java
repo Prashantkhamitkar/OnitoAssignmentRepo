a6dae1e3f47e5b8a2eb1946fd11aa5833d396b50
prashantkhamitkar76@gmail.com <prashantkhamitkar@gmail.com>
2024-01-26 13:45:38 +0530

    Adding globalException handling

--- OnitoAssignment/onitoassignment/src/main/java/com/onito/globalexception/GlobalExceptionClass.java
+++ OnitoAssignment/onitoassignment/src/main/java/com/onito/globalexception/GlobalExceptionClass.java
@@ -1,9 +1,11 @@
 package com.onito.globalexception;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -18,6 +20,9 @@ public class GlobalExceptionClass extends ResponseEntityExceptionHandler {
 		return super.handleMethodArgumentNotValid(ex, headers, status, request);
 	}
 	
-	
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<?> handleRuntimeException(RuntimeException e){
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+	}
 
 }
